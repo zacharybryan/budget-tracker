c7318ae61e45c13c9a64e4cf1dcc4b9a5dc63a5b
Zach Bryan <zacharybryan@icloud.com>
2020-11-19 15:32:23 -0800

    deploy bug

--- server.js
+++ server.js
@@ -16,11 +16,8 @@ app.use(express.json());
 app.use(express.static("public"));
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/budget',
-  {
+  process.env.MONGODB_URI, {
     useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
     useFindAndModify: false
   }
 );
